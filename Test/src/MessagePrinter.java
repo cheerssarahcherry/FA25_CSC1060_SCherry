149797029ae4a3aa47bbd293ec4039ba8081feaa
SaM <cheers.sarahcherry@gmail.com>
2025-11-28 14:42:57 -0800

    cleanup

--- Test/src/MessagePrinter.java
+++
@@ -1,9 +0,0 @@
-
-public class MessagePrinter {
-
-	public static void main(String[] args) {
-	// Call the script MessagePrinter. Write a program to print the message “This is my first programming test” to the screen. 10 pts
-	System.out.println("This is my first programming test!");
-
-	}
-}
